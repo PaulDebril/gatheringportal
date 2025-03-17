265d00dd324efa93892faef914b2768ad49cd1d3
Paul Debril <debril.paul@gmail.com>
2025-03-17 16:11:46 +0100

    fix(vercel) : fix vercel deploy

--- src/app/(pages)/actualites/[id]/page.tsx
+++ src/app/(pages)/actualites/[id]/page.tsx
@@ -1,12 +1,7 @@
 import ArticleDetail from '@/app/components/Article/ArticleDetail'
 
-interface ArticleDetailPageProps {
-  params: { id: string }
-}
-
-export default function ArticleDetailPage({ params }: ArticleDetailPageProps) {
-  console.log("ID reçu:", params.id)
-
+export default function ArticleDetailPage({ params }: { params: { id: string } }) {
+  console.log(params.id)
   return (
     <main className="pt-16">
       <ArticleDetail id={params.id} />
