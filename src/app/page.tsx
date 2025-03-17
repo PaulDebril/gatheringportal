5a849230991b6eeaee4d1a89fc3c8662031e2cb4
Marine Langrez <115779031+woorzz@users.noreply.github.com>
2025-03-17 14:48:24 +0100

    feat(home) : Add component ArticleList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
+"use client";
+import React from "react";
+import ArticleList from "@/app/components/Article/ArticleList";
+import Title from "./components/Title";
+import Newsletter from "./components/Newsletter/page";
+
 export default function Home() {
+
   return (
     <main>
-      <div className="relative h-screen">
-        <div className="absolute inset-0 blur-sm bg-cover" style={{ backgroundImage: "url('images/bg2.png')" }}></div>
+      <section className="relative h-screen">
+        <div
+          className="absolute inset-0 blur-sm bg-cover"
+          style={{ backgroundImage: "url('images/bg2.png')" }}
+        ></div>
         <div className="absolute inset-0 bg-black/70"></div>
+
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-5xl font-bold text-white">Gathering Portal</h1>
+          <h1 className="text-7xl font-bold text-[#e2b155] beleren-font max-w-3xl text-center">
+            Explorer l'univers de Magic The Gathering
+          </h1>
         </div>
-      </div>
-      
+      </section>
+      <section className="py-10">
+        <div className="flex flex-col items-center pt-5">
+          <Title text="ACTUALITES" />
+          <ArticleList />
+        </div>
+      </section>
+     
     </main>
-  )
+  );
 }
